4c665bc9eb8fa8f8fa7111a0c620eff5797b3f6e
jacashira <jacashira@gmail.com>
2024-05-26 11:50:23 -0600

    commit con axios consume ok y resto de navigacion desarrollada

--- src/main.js
+++ src/main.js
@@ -1,16 +1,24 @@
+const api = axios.create({
+    baseURL : 'https://api.themoviedb.org/3/' ,
+    Headers: {
+        'Content-Type':'application/json;charset=utf-8'
+    },
+    params: {
+        'api_key': API_KEY,
+    }
+})
+
+//ulitls methods
 
 
-async function getTrendingMoviesPreview () {
-    const res = await fetch('https://api.themoviedb.org/3/trending/movie/day?api_key='+ API_KEY)
-    const data = await res.json()
-
-    const movies = data.results
-    const moviesContainerList = document.getElementById('trendingMovies')
-    
+function createMovies(movies, container) {
+    container.innerHTML = ""
     movies.forEach(movie => {
         const movieContainer = document.createElement('div')
         movieContainer.classList.add('movie-container')
-
+        movieContainer.addEventListener('click',() => {
+            location.hash =  '#movie=' + movie.id 
+        })
         const moviePicture = document.createElement('img')
         moviePicture.classList.add('movie-img')
         moviePicture.alt = movie.title
@@ -20,37 +28,103 @@ async function getTrendingMoviesPreview () {
        
         movieContainer.appendChild(moviePicture)
 
-        moviesContainerList.appendChild(movieContainer)
+        container.appendChild(movieContainer)
     });
 
 }
 
-async function getCategoriesPreview() {
-    
-    const res = await fetch('https://api.themoviedb.org/3/genre/movie/list?api_key='+ API_KEY)
-    const data = await res.json()
-    console.log(data);
-
-    const genres = data.genres
-
-    const categoriesContainerList = document.getElementById('categoriesPreviewContainer')
-
-    genres.forEach(category => {
+function createCategories(categories,container){
+    container.innerHTML = ""
+    categories.forEach(category => {
         const categoryContainer = document.createElement('div')
         categoryContainer.classList.add('category-container')
-
+        
         const categoryTitle = document.createElement('h3')
         categoryTitle.classList.add('category-title')
         categoryTitle.id = 'id' + category.id
+
+        categoryTitle.addEventListener('click',() => {
+            location.hash = '#category=' + category.id + '-' + category.name
+        })
 
         const categoryTitleText = document.createTextNode(category.name)
 
         categoryTitle.appendChild(categoryTitleText)
         categoryContainer.appendChild(categoryTitle)
 
-        categoriesContainerList.appendChild(categoryContainer)
+        container.appendChild(categoryContainer)
     });
 }
 
-getTrendingMoviesPreview()
-getCategoriesPreview()
+// api methods
+
+async function getTrendingMoviesPreview () {
+    const { data } =  await api('trending/movie/day')
+    const movies = data.results
+    createMovies(movies,trendingMoviesPreviewList)
+}
+
+async function getCategoriesPreview() {
+    const { data } =  await api('genre/movie/list')
+
+    const genres = data.genres
+    createCategories(genres,categoriesPreviewList)
+}
+
+async function getMoviesByCategory (id) {
+    const { data } =  await api('discover/movie',{
+        params: {
+            with_genres: id,
+        }
+    })
+    const movies = data.results
+    createMovies(movies,genericSection)
+}
+
+async function getMoviesBySearch (query) {
+    const { data } =  await api('search/movie',{
+        params: {
+            query: query,
+        }
+    })
+    const movies = data.results
+    createMovies(movies,genericSection)
+}
+
+async function geTrendingMovies () {
+    const { data } =  await api('trending/movie/day')
+    const movies = data.results
+    createMovies(movies,genericSection)
+}
+
+async function getMovieById (id) {
+    const { data: movie } =  await api('movie/'+ id)
+
+    const movieImg = 'https://image.tmdb.org/t/p/w500' + movie.poster_path
+
+    headerSection.style.background = `
+    linear-gradient(
+        180deg, 
+        rgba(0, 0, 0, 0.35) 19.27%, 
+        rgba(0, 0, 0, 0) 29.17%
+        ),
+        url(${movieImg})` 
+        
+    movieDetailTitle.textContent = movie.title
+    movieDetailDescription.textContent = movie.overview
+    movieDetailScore.textContent = movie.vote_average
+    getRelatedMoviesId(id)
+    createCategories(movie.genres,movieDetailCategoriesList)
+}
+
+async function getRelatedMoviesId(id) {
+    const { data } = await api(`movie/${id}/recommendations`)
+    const relatedMovies = data.results
+
+    createMovies(relatedMovies,relatedMoviesContainer)
+ }
+
+
+
+
+
